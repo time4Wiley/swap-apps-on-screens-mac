c1100289d5cb4e4295fd35534a41e8114753c227
Wei Sun <sunwei415@126.com>
2025-08-12 15:52:08 +0800

    scafold a new project for the same feature in Objective-C.

---
+++ SizeUpSwapperInObjc/SizeUpSwapperInObjc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SizeUpSwapperInObjc
+//
+//  Created by Wei Sun on 2025/8/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
